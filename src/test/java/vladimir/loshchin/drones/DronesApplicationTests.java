e87ec4525a8041ee613b97c99af8d5075407f063
Vladimir Loshchin <vladimirloshchin@cloud.upwork.com>
2022-10-26 15:19:53 +0600

    Implement basic drone load tests

--- src/test/java/vladimir/loshchin/drones/DronesApplicationTests.java
+++ src/test/java/vladimir/loshchin/drones/DronesApplicationTests.java
@@ -1,13 +1,63 @@
 package vladimir.loshchin.drones;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import java.net.URI;
+import java.net.URISyntaxException;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.RequestEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpMethod.PUT;
+import static org.springframework.http.HttpStatus.NOT_FOUND;
+import static org.springframework.http.HttpStatus.OK;
+
+@SpringBootTest(webEnvironment=SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DronesApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    private TestRestTemplate restTemplate;
 
+    @LocalServerPort
+    private int port;
+
+
+    @Test
+    void loadUnexistentDrone() throws URISyntaxException {
+        var resp = restTemplate.exchange(
+            put("/drone/NOT-EXIST/load/ASPIRIN"), Void.class);
+
+        assertEquals(NOT_FOUND, resp.getStatusCode());
+    }
+
+    @Test
+    void loadDroneWithUnexistendMedication() throws URISyntaxException {
+        var resp = restTemplate.exchange(
+            put("/drone/SERIAL-1/load/NOT-EXIST"), Void.class);
+
+        assertEquals(NOT_FOUND, resp.getStatusCode());
+    }
+
+    @Test
+    void loadDrone() {
+        var resp = restTemplate.exchange(
+            put("/drone/SERIAL-1/load/ASPIRIN"), Void.class);
+
+        assertEquals(OK, resp.getStatusCode());
+    }
+
+    private RequestEntity<?> put(String path) {
+        return new RequestEntity<>(PUT, uri(path));
+    }
+
+    private URI uri(String path) {
+        try {
+            return new URI(String.format("http://localhost:%d%s", port, path));
+        } catch (URISyntaxException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
 }
